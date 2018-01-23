3444a7cc21634bba2e3f88e372d04b7fbcd26280
Robert Clayton <robbie.c.clayton@gmail.com>
2018-01-23 22:57:29 +0000

    refactored according to rubocop

--- spec/disappearing_kittens_spec.rb
+++ spec/disappearing_kittens_spec.rb
@@ -1,9 +1,6 @@
 describe DisappearingKittens do
-  let(:directions) { %w[forward right forward forward forward left forward
-                        forward left right forward right forward forward right
-                        forward forward left] }
-
-  it 'test' do
-
+  let(:directions) do
+    %w[forward right forward forward forward left forward forward left right
+       forward right forward forward right forward forward left]
   end
 end
