9dd8e2e30218e43140de73d1d7ae05104a642395
Robert Clayton <robbie.c.clayton@gmail.com>
2018-01-23 21:20:29 +0000

    gets the directions from the API

--- lib/disappearing_kittens.rb
+++ lib/disappearing_kittens.rb
@@ -1,2 +1,7 @@
 class DisappearingKittens
+  def get_directions
+    uri = URI('http://which-technical-exercise.herokuapp.com/api/:robbie.c.clayton@gmail.com/directions')
+    directions = Net::HTTP.get(uri)
+    eval(directions)
+  end
 end
