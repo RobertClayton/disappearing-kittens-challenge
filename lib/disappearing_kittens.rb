13b5f80d8985f98a879aec5c27640cc3546afcab
Robert Clayton <robbie.c.clayton@gmail.com>
2018-01-23 23:03:21 +0000

    stores an array for current location

--- lib/disappearing_kittens.rb
+++ lib/disappearing_kittens.rb
@@ -2,11 +2,12 @@ require './lib/api_interraction.rb'
 
 # This class is controller to find the disappearing Kittens
 class DisappearingKittens
-  attr_reader :directions
+  attr_reader :directions, :current_location
 
   def initialize
     @api_interraction = ApiInterraction.new
     @directions = []
+    @current_location = [0, 0]
   end
 
   def retrieve_directions
