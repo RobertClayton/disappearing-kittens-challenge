cc50d494881e5254959977b4991d32acc3c92d8e
Robert Clayton <robbie.c.clayton@gmail.com>
2018-01-23 22:01:31 +0000

    saves the directions as an array to #directions

--- lib/disappearing_kittens.rb
+++ lib/disappearing_kittens.rb
@@ -1,8 +1,14 @@
 # This class is controller to find the disappearing Kittens
 class DisappearingKittens
+  attr_reader :directions
+
+  def initialize
+    @directions = []
+  end
+
   def retrieve_directions
     uri = URI('http://which-technical-exercise.herokuapp.com/api/:robbie.c.clayton@gmail.com/directions')
-    directions = Net::HTTP.get(uri)
-    JSON.parse directions.gsub('=>', ':')
+    api_request = JSON.parse Net::HTTP.get(uri).gsub('=>', ':')
+    @directions = api_request['directions']
   end
 end
--- spec/disappearing_kittens_spec.rb
+++ spec/disappearing_kittens_spec.rb
@@ -1,15 +1,15 @@
 describe DisappearingKittens do
   describe '#get_directions' do
-    it 'returns a Hash from the API get request' do
-      expect(subject.retrieve_directions).to be_an_instance_of(Hash)
+    it 'returns an array from the API get request' do
+      expect(subject.retrieve_directions).to be_an_instance_of(Array)
     end
 
     it 'saves the hash to the instance variable directions' do
-      expect(subject.directions).to be_an_instance_of(Hash)
+      subject.retrieve_directions
+      expect(subject.directions).to be_an_instance_of(Array)
       expect(subject.directions).to eq(
-        directions: %w[forward right forward forward forward left forward
-                       forward left right forward right forward forward right
-                       forward forward left]
+        %w[forward right forward forward forward left forward forward left right
+           forward right forward forward right forward forward left]
       )
     end
   end
