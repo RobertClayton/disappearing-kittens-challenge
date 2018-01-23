77da11a1adfb1fca369266a470ee9c7dee979194
Robert Clayton <robbie.c.clayton@gmail.com>
2018-01-23 23:18:20 +0000

    #direction_value stores values for each direction

--- lib/disappearing_kittens.rb
+++ lib/disappearing_kittens.rb
@@ -2,13 +2,20 @@ require './lib/api_interraction.rb'
 
 # This class is controller to find the disappearing Kittens
 class DisappearingKittens
-  attr_reader :directions, :current_location, :current_direction
+  attr_reader :directions, :current_location, :current_direction,
+              :direction_value
 
   def initialize
     @api_interraction = ApiInterraction.new
     @directions = []
     @current_location = [0, 0]
     @current_direction = 'north'
+    @direction_value = {
+      'north' => [0, 1],
+      'east' => [1, 0],
+      'south' => [0, -1],
+      'west' => [-1, 0],
+    }
   end
 
   def retrieve_directions
