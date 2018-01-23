56b288b76653b2c9181f1f9cd113a2064a7a9cc8
Robert Clayton <robbie.c.clayton@gmail.com>
2018-01-23 23:02:04 +0000

    added test for #current_location

--- spec/disappearing_kittens_spec.rb
+++ spec/disappearing_kittens_spec.rb
@@ -3,4 +3,10 @@ describe DisappearingKittens do
     %w[forward right forward forward forward left forward forward left right
        forward right forward forward right forward forward left]
   end
+
+  describe '#current_location' do
+    it 'returns the current location in the format [x, y]' do
+      expect(subject.current_location).to eq([0, 0])
+    end
+  end
 end
