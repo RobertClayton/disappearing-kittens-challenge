c71aec49dfd3aad11d0ae41bbc7c8c21c8bb9de8
Robert Clayton <robbie.c.clayton@gmail.com>
2018-01-23 23:07:57 +0000

    current direction defaults to north

--- lib/disappearing_kittens.rb
+++ lib/disappearing_kittens.rb
@@ -2,12 +2,13 @@ require './lib/api_interraction.rb'
 
 # This class is controller to find the disappearing Kittens
 class DisappearingKittens
-  attr_reader :directions, :current_location
+  attr_reader :directions, :current_location, :current_direction
 
   def initialize
     @api_interraction = ApiInterraction.new
     @directions = []
     @current_location = [0, 0]
+    @current_direction = 'north'
   end
 
   def retrieve_directions
